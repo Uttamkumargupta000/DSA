e68e5a45d396485973734120306256077871552f
uttam Kumar <uttamkumargupta000@gmail.com>
2024-05-24 06:51:33 +0530

    leet code question added

---
+++ Leetcode/1281.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int sum=0;
+        int prod=1;
+        while(n!=0){
+            int rem=n%10;
+            sum+=rem;
+            prod*=rem;
+            n=n/10;
+        }
+        int answer=prod-sum;
+        return answer;
+    }
+};
