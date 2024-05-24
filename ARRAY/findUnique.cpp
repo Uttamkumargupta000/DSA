3c2d82d3af9473db85675ac55a22a80ed3089a3b
uttam Kumar <uttamkumargupta000@gmail.com>
2024-05-25 01:38:56 +0530

    question added

---
+++ ARRAY/findUnique.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int findUnique(int arr[], int n){
+    int ans=0;
+    for(int i=0;i<n;i++){
+        ans=ans^arr[i];
+    }
+    return ans;
+}
+void printArray(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int size;
+    cout<<"Enter the value of size : ";
+    cin>>size;
+    int arr[100];
+    //taking input to array
+    for(int i=0;i<size;i++){
+        cout<<"The value at the index "<< i << " is : ";
+        cin>>arr[i];
+    }
+    printArray(arr,size);
+    cout<<findUnique(arr,size)<<endl;
+    printArray(arr,size);
+    return 0;
+}
