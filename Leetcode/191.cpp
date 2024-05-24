90fec44cbebce766ab3cdf04fe15d9498e37212a
uttam Kumar <uttamkumargupta000@gmail.com>
2024-05-24 16:59:15 +0530

    Leetcode Question added

--- Leetcode/191.cpp
+++ Leetcode/191.cpp
@@ -1,3 +1,8 @@
+/* Write a function that takes the binary representation of a positive integer and returns the number of
+set bits
+it has (also known as the Hamming weight).
+*/
+
 class Solution {
 public:
     int hammingWeight(int n) {
