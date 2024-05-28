8b89a6ac79c114ffc619212a0e5adc9ae3d337d2
uttam Kumar <uttamkumargupta000@gmail.com>
2024-05-29 02:08:29 +0530

    code added

---
+++ Leetcode/344.cpp
@@ -0,0 +1,22 @@
+/*     Reverse String     */
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        /* also this line also do same reversal process
+
+        reverse(s.begin(),s.end());
+
+        */
+        int i = 0;
+        int j = s.size()-1;
+        while(i < j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+};
