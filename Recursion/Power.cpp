53566db6b3721431e30ad2a5bef349bcd682e9e8
uttam Kumar <uttamkumargupta000@gmail.com>
2024-06-03 19:09:52 +0530

    code modofied

--- Recursion/Power.cpp
+++ Recursion/Power.cpp
@@ -1,17 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 int power(int n){
+    if(n==0)
+        return 1;
     
+    return 2*power(n-1);
 }
 
 int main(){
 
     int n;
-    cout<<"The value of n : ";
+    cout<<"Enter the  value of n : ";
     cin >> n;
-    cout<<"the factorial of  "<< n <<" : "<<factorial(n)<<endl;
+    cout<<"The  2 power of  "<< n <<" : "<<power(n)<<endl;
 
     return 0;
 }
