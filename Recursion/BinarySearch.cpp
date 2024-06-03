4bdcf168e96e9c81065e03ba63ec02cc7528a7cb
uttam Kumar <uttamkumargupta000@gmail.com>
2024-06-04 03:06:50 +0530

    code modified

--- Recursion/BinarySearch.cpp
+++ Recursion/BinarySearch.cpp
@@ -1,13 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void printArray(int arr[], int s, int e){
+
+    for(int i=s;i<=e;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+}
+
 bool BinarySearch(int arr[], int s, int e, int key){
+    cout<<endl;
+    printArray(arr,s,e);
     if(s>e){
         return 0;
     }
 
     int mid= s+(e-s)/2;
-
+    cout<<"value of arr mid is "<<arr[mid]<<endl;
     if(arr[mid]==key)
         return 1;
     
@@ -25,12 +34,12 @@ int main(){
     int size=6;
     int key=6;
 
-    bool ans= BinarySearch(arr,0,size-1, key);
-    if(ans){
-        cout<<"Key is present " <<endl;
-    }
-    else{
-        cout<<"Key is not present "<<endl;
-    }
+    cout<<"present or not "<< BinarySearch(arr,0,size-1, key)<<endl;
+    // if(ans){
+    //     cout<<"Key is present " <<endl;
+    // }
+    // else{
+    //     cout<<"Key is not present "<<endl;
+    // }
     return 0;
 }
