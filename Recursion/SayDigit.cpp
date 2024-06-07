8466de83cf4d4650ce0ad812536dbdfded0fafe9
uttam Kumar <uttamkumargupta000@gmail.com>
2024-06-07 10:22:09 +0530

    code added

---
+++ Recursion/SayDigit.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sayDigit(int n, string arr[]){
+    if(n==0){
+        return ;
+    }
+    int digit = n%10;
+    n = n/10;
+
+    sayDigit(n, arr);
+    
+    cout<<arr[digit]<<" ";
+}
+int main(){
+
+    string arr[10]={"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    int n;
+    cout<<"Enter the number : ";
+    cin >> n;
+    
+    sayDigit(n,arr);
+    return 0;
+}
