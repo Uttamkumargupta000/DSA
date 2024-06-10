0a4c3c56f6435926aafab406ea69a76e32ce0b46
uttam Kumar <uttamkumargupta000@gmail.com>
2024-06-10 22:44:16 +0530

    1st pillar

---
+++ OOPS/Encapsulation.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// inforamtion/data hiding hence security improves
+class student{
+
+    private:
+    string name;
+    int age;
+    int height;
+    
+    public:
+    int getAge(){
+        return this->age;
+    }
+};
+
+int main(){
+
+    student first;
+    
+    cout<<"good working "<<endl;
+    return 0;
+}
