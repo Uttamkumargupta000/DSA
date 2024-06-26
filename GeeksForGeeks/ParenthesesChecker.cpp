1950cae60399588354b3a8f07570c71c2652b608
uttam Kumar <uttamkumargupta000@gmail.com>
2024-06-26 11:35:26 +0530

    Parentheses_checker

---
+++ GeeksForGeeks/ParenthesesChecker.cpp
@@ -0,0 +1,44 @@
+/* Parentheses checker */
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        stack<char> s;
+        for(int i =0 ;i<x.length() ;i++){
+            char ch = x[i];
+            
+            //for opening bracket
+            if(ch == '(' || ch == '{' || ch == '['){
+                s.push(ch);
+            }
+            
+            else{
+                
+                //for closing bracket
+                if(!s.empty()){
+                    char top = s.top();
+                    
+                    if((ch == ')' && top == '(') || (ch == '}' && top == '{') ||( ch == ']' && top == '[')){
+                        s.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(s.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+};
