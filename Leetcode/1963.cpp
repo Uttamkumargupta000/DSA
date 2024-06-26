d72cce2831eac0bbae59ddc0e05adecb3d787675
uttam Kumar <uttamkumargupta000@gmail.com>
2024-06-26 20:27:31 +0530

    Minumum_Number_to_make_string_balanced

---
+++ Leetcode/1963.cpp
@@ -0,0 +1,47 @@
+/* Minimum Number of Swaps to Make the String Balanced */
+
+class Solution {
+public:
+    int minSwaps(string s) {
+        
+    //odd condition
+      if(s.length()%2 == 1) {
+        return -1;
+    }
+    
+    stack<char> st;
+    for(int i=0; i<s.length(); i++) {
+        char ch = s[i];
+        
+        if(ch == '[') 
+            st.push(ch);
+		else
+        {
+            //ch is closed brace
+            if(!st.empty() && st.top() == '[' && ch == ']') {
+                st.pop();
+            }
+            else
+            {
+                st.push(ch);
+            }
+        }
+    }
+    
+        //stack contains invalid expression
+        int a = 0, b = 0;
+        while(!st.empty()) {
+            if(st.top() == '[') {
+                b++;
+            }
+            else
+            {
+                a++;
+            }
+            st.pop();
+        }
+        
+		int ans = (a+1)/2 + (b+1)/2;
+        return ans/2;
+    }
+};
